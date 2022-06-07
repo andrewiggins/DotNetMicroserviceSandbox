2121f9f7369e6e36994c7267998da9ec52fb68e5
Andre Wiggins <andwi@microsoft.com>
2022-06-07 15:50:52 -0700

    Login using MSAL.JS

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/// <reference types="@azure/msal-browser" />
+/** @type {import("@azure/msal-browser")} */
+var msal = window.msal;
 
-// Write your JavaScript code.
+const publicClient = new msal.PublicClientApplication({
+  auth: {
+    clientId: "8a612ea2-4d70-4837-a69d-d2a003384c37",
+  },
+});
+
+publicClient.handleRedirectPromise(location.hash).then((result) => {
+  if (result) {
+    console.log("Auth redirect response:", result);
+    publicClient.setActiveAccount(result.account);
+    initMSALLogin();
+  }
+});
+
+initMSALLogin();
+
+function initMSALLogin() {
+  const msalContainer = document.getElementById("msal-login");
+
+  const activeAccount = publicClient.getActiveAccount();
+  if (activeAccount) {
+    msalContainer.innerHTML = `
+      <p>MSAL.JS logged in as ${activeAccount.name}.</p>
+    `;
+  } else {
+    msalContainer.innerHTML = `<button class="btn btn-primary" onclick="onLogin()">Login</button>`;
+  }
+}
+
+function onLogin() {
+  publicClient.loginRedirect();
+}
