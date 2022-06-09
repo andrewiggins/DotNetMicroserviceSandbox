c6f060a058329979aa10d5f6977c72abe8642282
Andre Wiggins <andwi@microsoft.com>
2022-06-09 09:00:01 -0700

    Call UserPref service from Forecast service

--- ForecastMicroservice/Program.cs
+++ ForecastMicroservice/Program.cs
@@ -1,3 +1,4 @@
+using ForecastMicroservice;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
@@ -7,13 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+    .AddMicrosoftIdentityWebApi(builder.Configuration)
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
 builder.Services.AddAuthorization();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader());
 });
 
+builder.Services.AddHttpClient<UserPreferencesClient>();
 
 var app = builder.Build();
 
@@ -32,19 +36,32 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+app.MapGet("/weatherforecast", async (HttpContext httpContext, UserPreferencesClient userPreferencesClient) =>
 {
     httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
 
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var userPrefs = await userPreferencesClient.GetUserPreferences();
+    if (userPrefs == null)
+    {
+        return null;
+    }
+
+    var cityForecasts = new Dictionary<string, WeatherForecast[]>();
+    foreach (var city in userPrefs.Cities)
+    {
+        var forecast = Enumerable.Range(1, 3).Select(index =>
+            new WeatherForecast
+            (
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+
+        cityForecasts[city] = forecast;
+    }
+
+    return cityForecasts;
 })
 .RequireAuthorization();
 
---
+++ ForecastMicroservice/UserPreferencesClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Web;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
+
+namespace ForecastMicroservice
+{
+    public class UserPreferencesClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly ITokenAcquisition tokenAcquisition;
+        private readonly string[] Scopes = new string[]
+        {
+            "api://b45d5468-bd83-4787-a644-07747ea2d602/access_as_user"
+        };
+
+        public UserPreferencesClient(ITokenAcquisition tokenAcquisition, HttpClient httpClient)
+        {
+            this.tokenAcquisition = tokenAcquisition;
+            this.httpClient = httpClient;
+            this.httpClient.BaseAddress = new Uri("https://localhost:7072/");
+        }
+
+        public async Task<UserPreferences?> GetUserPreferences()
+        {
+            // Inspired by https://github.com/AzureAD/microsoft-identity-web/blob/bba91c4298411e780c9f082e8e3843eacd8e543c/src/Microsoft.Identity.Web.MicrosoftGraph/TokenAcquisitionAuthenticationProvider.cs#L60
+            // and https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0
+            var accessToken = await this.tokenAcquisition.GetAccessTokenForUserAsync(this.Scopes).ConfigureAwait(false);
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "userprefs");
+            httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue(Constants.Bearer, accessToken);
+
+
+            //httpClient.GetFromJsonAsync
+            var response = await this.httpClient.SendAsync(httpRequestMessage);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UserPreferences>();
+        }
+    }
+
+    public record UserPreferences(ICollection<string> Cities);
+}
